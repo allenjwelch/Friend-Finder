96e210470e682685206c8f9dcd64c1b7c3451a66
Allen Welch <allenjwelch@gmail.com>
2018-05-07 19:37:12 -0400

    added routes

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+let friendsArray = [
+  {
+    name: "allen",
+    email: 'allen@gmail.com',
+    photo: "image.png",
+    surveyData: [1, 2, 3, 4, 5]
+  },
+  {
+    name: "susan",
+    email: 'susan@gmail.com',
+    photo: "image.png",
+    surveyData: [1, 2, 3, 4, 5]
+  },
+  {
+    name: "eddie",
+    email: 'eddie@gmail.com',
+    photo: "image.png",
+    surveyData: [1, 2, 3, 4, 5]
+  },
+];
+
+module.exports = friendsArray; 
---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+// const $ = require('jquery'); 
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app); 
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
