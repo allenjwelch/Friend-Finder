e1d969478b97c83f861c6c2d98666923f0529653
Allen Welch <allenjwelch@gmail.com>
2018-05-08 18:24:41 -0400

    created logic for friend finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
-let friendsArray = [
+let friendsData = [
   {
     name: "allen",
     email: 'allen@gmail.com',
     photo: "image.png",
-    surveyData: [1, 2, 3, 4, 5]
+    surveyData: [1, 1, 1, 1, 1]
   },
   {
     name: "susan",
@@ -19,4 +19,39 @@ let friendsArray = [
   },
 ];
 
-module.exports = friendsArray; 
+
+// Friend Finder Logic =========================================
+let newUser = friendsData[friendsData.length -1].surveyData; 
+console.log(newUser); 
+let matchTotals = [];
+let bestMatch = ''; 
+
+for (let i = 0; i < friendsData.length - 1; i++) {
+  let match =[];
+  let totals = 0;
+  for (let x =0; x < 5; x++) { 
+    match.push(Math.abs(newUser[x] - friendsData[i].surveyData[x])); 
+  }
+
+  match.forEach(score => {
+    totals += score; 
+  })
+
+  let tempArr = []; 
+  tempArr.push(i, totals); 
+  matchTotals.push(tempArr); 
+
+  bestMatch = matchTotals.sort(function(a, b){
+    return b[1] > a[1] ? -1 : 1; 
+  });
+  
+  console.log(bestMatch[0][0]); 
+  let bestMatchIndex = parseFloat(bestMatch[0][0]);
+  console.log(friendsData[bestMatchIndex].name); 
+}
+
+
+
+
+
+module.exports = friendsData; 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,11 @@ module.exports = function (app) {
   app.get('/api/friends', function(req, res) {
     res.json(friendsData); 
   }); 
-}
+
+  app.post('/api/friends', function(req, res) {
+    friendsData.push(req.body); 
+    res.json(true); // NOT SURE IF NEEDED
+
+  }); 
+}; //module.exports
+
