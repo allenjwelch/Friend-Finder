94233b24db14aa48d5dcaaa9936267c944697797
Allen Welch <allenjwelch@gmail.com>
2018-05-10 11:55:35 -0400

    updated survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,38 +20,5 @@ let friendsData = [
 ];
 
 
-// Friend Finder Logic =========================================
-let newUser = friendsData[friendsData.length -1].surveyData; 
-console.log(newUser); 
-let matchTotals = [];
-let bestMatch = ''; 
-
-for (let i = 0; i < friendsData.length - 1; i++) {
-  let match =[];
-  let totals = 0;
-  for (let x =0; x < 5; x++) { 
-    match.push(Math.abs(newUser[x] - friendsData[i].surveyData[x])); 
-  }
-
-  match.forEach(score => {
-    totals += score; 
-  })
-
-  let tempArr = []; 
-  tempArr.push(i, totals); 
-  matchTotals.push(tempArr); 
-
-  bestMatch = matchTotals.sort(function(a, b){
-    return b[1] > a[1] ? -1 : 1; 
-  });
-  
-  console.log(bestMatch[0][0]); 
-  let bestMatchIndex = parseFloat(bestMatch[0][0]);
-  console.log(friendsData[bestMatchIndex].name); 
-}
-
-
-
-
 
 module.exports = friendsData; 
