fa83726350344ccacaec2c24c21bb6edb07bc96f
Allen Welch <allenjwelch@gmail.com>
2018-05-10 14:16:17 -0400

    completed logic for friendFinder and updated modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,21 @@
 let friendsData = [
   {
-    name: "allen",
+    name: "Allen",
     email: 'allen@gmail.com',
-    photo: "image.png",
-    surveyData: [1, 1, 1, 1, 1]
+    photo: "https://media.giphy.com/media/tDFliI4pFgoE0/source.gif",
+    surveyData: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   {
-    name: "susan",
+    name: "Susan",
     email: 'susan@gmail.com',
-    photo: "image.png",
-    surveyData: [1, 2, 3, 4, 5]
+    photo: "https://78.media.tumblr.com/a693944928f30349b5ad40c335e86087/tumblr_ntq5upIrYT1uerigno1_400.gif",
+    surveyData: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   },
   {
-    name: "eddie",
+    name: "Eddie",
     email: 'eddie@gmail.com',
-    photo: "image.png",
-    surveyData: [1, 2, 3, 4, 5]
+    photo: "https://i.pinimg.com/originals/4c/fa/78/4cfa7893f130114cdd687a99ae4ac558.gif",
+    surveyData: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
 ];
 
