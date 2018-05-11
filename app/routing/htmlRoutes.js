3b54d097b3b6c856869cbb90f553fefb63916f1e
Allen Welch <allenjwelch@gmail.com>
2018-05-10 20:56:59 -0400

    resolved image issue

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,11 +10,11 @@ module.exports = function (app) {
   // });
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
   
-  app.use( function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   // app.use('/static', express.static(path.join(__dirname, 'public')))
--- server.js
+++ server.js
@@ -14,8 +14,13 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('/app/public/images'));
-// app.use('/public', express.static(path.join(__dirname, 'public'))) 
+
+app.use(express.static('imgs'));
+// app.use('/public', express["static"](path.join(__dirname, 'images')));
+// app.use('/app', express.static(path.join(__dirname, 'public'))) 
+// app.use('/static', express.static(path.join(__dirname + 'public')));
+// app.use(express.static(path.resolve('./public')));
+// app.use('/public', express.static(path.resolve('./public'))); //<--new line added
 
 require('./app/routing/apiRoutes.js')(app); 
 require('./app/routing/htmlRoutes.js')(app); 
